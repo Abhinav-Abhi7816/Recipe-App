9a5aed34d2cae261ddaca0eceb32673895908be7
ABHINAV ABHI <Abhinav Thonti>
2024-12-17 11:06:17 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, createRoutesFromElements, Route,RouterProvider } from 'react-router-dom';
@@ -26,4 +27,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+reportWebVitals();
