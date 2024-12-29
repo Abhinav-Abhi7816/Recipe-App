c6cca008dc2f164db904e379794ab7aa9834efb2
ABHINAV ABHI <Abhinav Thonti>
2024-12-29 18:44:24 +0530

    Updated the website with new changes

--- src/Components/Favorites.js
+++ src/Components/Favorites.js
@@ -12,7 +12,7 @@ function Favorites() {
       <div className='md:pt-32 pt-24'>
         {(favArr && favArr.length > 0) ? <h1 className='w-[100%] font-semibold text-center md:text-2xl text-lg underline underline-offset-8' style={{ textShadow: "2px 4px 20px rgb(0,0,0,0.3)" }}>Favorite Recipies</h1> : null}
         <div className='flex flex-wrap justify-around gap-4 md:gap-10 pt-4 md:p-8'>
-          {(favArr && favArr.length > 0) ? favArr.map((el, index) => (<div className='bg-white overflow-hidden rounded-2xl md:w-[450px] gap-3 w-[95%] md:py-8 py-4 px-4 flex flex-col justify-center place-items-center hover:scale-105 duration-500 hover:drop-shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.3)" }} key={index}>
+          {(favArr && favArr.length > 0) ? favArr.map((el, index) => (<div className='bg-white overflow-hidden rounded-2xl md:w-[450px] md:gap-3 gap-0  w-[95%] md:py-8 py-4 px-4 flex flex-col justify-center place-items-center hover:scale-105 duration-500 hover:drop-shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.3)" }} key={index}>
             <div>
               <img src={el.image_url} alt={el.title} className='md:w-96 md:h-64 w-96 h-52 rounded-xl' />
             </div>
